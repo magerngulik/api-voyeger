8aee8291945a7350c0778f24ab2145a27d20dda3
magerngulik <magerngulik@gmail.com>
2023-06-11 00:36:38 +0700

    setelah di hubungan ke model

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -2,15 +2,102 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use DB;
+use Carbon\Carbon;
+// use Illuminate\Validation\Validator;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Validator;
+
 
 class ProductController extends BaseController
 {
     
     public function index(){
-        return "oke";
+        return DB::table("products")->paginate(10);
+       
+    }
+    public function singleItem(){
+        $products = DB::table("products")->where('id', request()->id)->get();
+        if ($products->isEmpty()) {
+            return response()->json([
+                "status" => "gagal",
+                "data" => $products,
+                "message" => "product tidak di temukan",
+            ],404);
+        } else {
+            return response()->json([
+                "status" => "berhasil",
+                "data" => $products->first()        
+            ],200);
+        }
+    }
+
+    public function create(){
+        $post = request()->post();
+        $validator = Validator::make($post, [
+            'product_name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'image' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' =>'gagal',
+                'message' => 'Validasi gagal',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+        $insetTable = DB::table('products')->insert([
+            'product_name' => $post['product_name'],
+            'description' => $post['description'],
+            'price' => $post['price'],
+            'stock' => $post['stock'],
+            'image' => $post['image'],
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        return response()->json(['message' => 'Data berhasil disimpan'], 200);
+    }
+
+    public function update(){
+        $id = request()->id;
+        $post = request()->post();
+        $validator = Validator::make($post, [
+            'product_name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'image' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' =>'gagal',
+                'message' => 'Validasi gagal',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+        $insetTable = DB::table('products')
+            ->where('id', $id)
+            ->update([
+            'product_name' => $post['product_name'],
+            'description' => $post['description'],
+            'price' => $post['price'],
+            'stock' => $post['stock'],
+            'image' => $post['image'],
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        return response()->json(['message' => 'Data berhasil di update'], 200);
+    }
+
+    public function delete(){
+        $id = request()->id;
+        $insetTable = DB::table('products')
+            ->where('id', $id)
+            ->delete();
+        return response()->json(['message' => 'Data berhasil di hapus'], 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,99 +24,15 @@ Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
 
-
 Route::group(['prefix' => 'api'], function () {
- 
-    Route::get('/products', function () {
-        return DB::table("products")->paginate(10);
-    });
-    Route::get('/products/{id}', function () {
-        $products = DB::table("products")->where('id', request()->id)->get();
-        if ($products->isEmpty()) {
-            return response()->json([
-                "status" => "gagal",
-                "data" => $products,
-                "message" => "product tidak di temukan",
-            ],404);
-        } else {
-            return response()->json([
-                "status" => "berhasil",
-                "data" => $products->first()        
-            ],200);
-        }
-    });
-    
-    Route::post('/products', function(){
-        $post = request()->post();
-        $validator = Validator::make($post, [
-            'product_name' => 'required',
-            'description' => 'required',
-            'price' => 'required',
-            'stock' => 'required',
-            'image' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json([
-                'status' =>'gagal',
-                'message' => 'Validasi gagal',
-                'errors' => $validator->errors()
-            ], 400);
-        }
-        $insetTable = DB::table('products')->insert([
-            'product_name' => $post['product_name'],
-            'description' => $post['description'],
-            'price' => $post['price'],
-            'stock' => $post['stock'],
-            'image' => $post['image'],
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-
-        return response()->json(['message' => 'Data berhasil disimpan'], 200);
-    });
-
-    Route::post('/products/{id}', function(){
-        $id = request()->id;
-        $post = request()->post();
-        $validator = Validator::make($post, [
-            'product_name' => 'required',
-            'description' => 'required',
-            'price' => 'required',
-            'stock' => 'required',
-            'image' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json([
-                'status' =>'gagal',
-                'message' => 'Validasi gagal',
-                'errors' => $validator->errors()
-            ], 400);
-        }
-        $insetTable = DB::table('products')
-            ->where('id', $id)
-            ->update([
-            'product_name' => $post['product_name'],
-            'description' => $post['description'],
-            'price' => $post['price'],
-            'stock' => $post['stock'],
-            'image' => $post['image'],
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-        return response()->json(['message' => 'Data berhasil di update'], 200);
-    });
-
-    Route::delete('/products/{id}', function(){
-        $id = request()->id;
-        $insetTable = DB::table('products')
-            ->where('id', $id)
-            ->delete();
-        return response()->json(['message' => 'Data berhasil di hapus'], 200);
-    });
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'singleItem']);
+    Route::post('/products/', [ProductController::class, 'create']);
+    Route::post('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'delete']);
 });
 
 
-Route::get('/products/action/', [ProductController::class, 'index']);
 
 
 
